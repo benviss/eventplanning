4923aeeb3e11e253055ea36f29152fce50f3101a
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-09-09 11:36:00 -0700

    add methods to calculate total event cost

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -5,7 +5,7 @@ public class Event {
   private String mBand;
   private String mFood;
   private Integer mDuration;
-  private Integer mEventCost;
+  private Integer mEventCost = 0;
 
   public Event(Integer _guests, String _venue, boolean _alcohol, String _band, String _food, Integer _duration) {
     mGuests = _guests;
@@ -15,10 +15,100 @@ public class Event {
     mFood = _food;
     mDuration = _duration;
   }
-
-
-
-
+  public Integer tallyGuestCost() {
+    Integer guestCost = 0;
+    if (getGuests() >= 250) {
+      guestCost = 500;
+    } else if (getGuests() >= 150){
+      guestCost = 250;
+    } else if (getGuests() >= 100){
+      guestCost = 200;
+    } else if (getGuests() >= 50){
+      guestCost = 175;
+    } else if (getGuests() >= 25){
+      guestCost = 150;
+    } else {
+      guestCost = 100;
+    }
+    return guestCost;
+  }
+  public Integer tallyVenueCost() {
+    Integer venueCost = 0;
+    switch (getVenue()) {
+      case "Patio":
+        venueCost = 500;
+        break;
+      case "Ballroom":
+        venueCost = 1000;
+        break;
+      case "Castle":
+        venueCost = 5000;
+        break;
+    }
+    return venueCost;
+  }
+  public Integer tallyBandCost() {
+    Integer bandCost = 0;
+    switch (getBand()) {
+      case "The Raging Alcoholics":
+        if(getAlcohol()) {
+          bandCost = 0;
+        } else {
+          bandCost = 500;
+        }
+        break;
+      case "Never Stoppers":
+        bandCost = 2000;
+        break;
+      case "The Slow Jazz/Metal/Classical Trio of Cool Music":
+        bandCost = 10000;
+        break;
+    }
+    return bandCost;
+  }
+  public Integer tallyFoodCost() {
+    Integer foodCost = 0;
+    switch (getFood()) {
+      case "Snacks":
+        foodCost = (getGuests()*5);
+        break;
+      case "Sandwhices":
+        foodCost = (getGuests()*10);
+        break;
+      case "The Full Spread":
+        foodCost = (getGuests()*20);
+        break;
+    }
+    return foodCost;
+  }
+  public Integer tallyDurationCost() {
+    Integer durationCost = 0;
+    if (getDuration() >= 420) {
+      durationCost = 500;
+    } else if (getDuration() >= 360){
+      durationCost = 250;
+    } else if (getDuration() >= 240){
+      durationCost = 200;
+    } else if (getDuration() >= 120){
+      durationCost = 175;
+    } else if (getDuration() >= 60){
+      durationCost = 150;
+    } else {
+      durationCost = 100;
+    }
+    return durationCost;
+  }
+  public Integer tallyEventCost() {
+    Integer eventCost = 0;
+    if(mAlcohol) {
+      eventCost += (getGuests() * 5);
+    }
+    eventCost += (tallyGuestCost() + tallyVenueCost() + tallyBandCost() + tallyFoodCost() + tallyDurationCost());
+    return eventCost;
+  }
+  public Integer getEventCost() {
+    return mEventCost;
+  }
   public Integer getGuests() {
     return mGuests;
   }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,14 +1,52 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.util.List;
-import java.util.ArrayList;
+
 
 public class EventTest {
-  Event newEvent = new Event(100,"Patio",true,"Ruckers","Lots",60);
+  Event newEvent = new Event(100,"Patio",true,"Never Stoppers","Snacks",60);
   @Test
   public void newEvent_instantiatesCorrectly_True() {
     assertEquals(true, newEvent instanceof Event);
   }
+  @Test
+  public void newEvent_returnsGuestCost_200() {
+    Integer expectedOutput = 200;
+    assertEquals(expectedOutput, newEvent.tallyGuestCost());
+  }
+  @Test
+  public void newEvent_returnsVenueCost_500() {
+    Integer expectedOutput = 500;
+    assertEquals(expectedOutput, newEvent.tallyVenueCost());
+  }
+  @Test
+  public void newEvent_returnsBandCost_2000() {
+    Integer expectedOutput = 2000;
+    assertEquals(expectedOutput, newEvent.tallyBandCost());
+  }
+  @Test
+  public void newEvent_returnsBandCostWithAlcohol_0() {
+    Event newEvent2 = new Event(100,"Patio",true,"The Raging Alcoholics","Snacks",60);
+    Integer expectedOutput = 0;
+    assertEquals(expectedOutput, newEvent2.tallyBandCost());
+  }
+  @Test
+  public void newEvent_returnsFoodCost_500() {
+    Integer expectedOutput = 500;
+    assertEquals(expectedOutput, newEvent.tallyFoodCost());
+  }
+  @Test
+  public void newEvent_returnsDurationCost_150() {
+    Integer expectedOutput = 150;
+    assertEquals(expectedOutput, newEvent.tallyDurationCost());
+  }
+  @Test
+  public void newEvent_returnsEventCost_500() {
+    Integer expectedOutput = 3850;
+    assertEquals(expectedOutput, newEvent.tallyEventCost());
+  }
+
+
+
   @Test
   public void newEvent_returnsGuestNumber_100() {
     Integer expectedOutput = 100;
@@ -25,12 +63,12 @@ public class EventTest {
     assertEquals(expectedOutput, newEvent.getAlcohol());
   }@Test
   public void newEvent_returnsBand_Ruckers() {
-    String expectedOutput = "Ruckers";
+    String expectedOutput = "Never Stoppers";
     assertEquals(expectedOutput, newEvent.getBand());
   }
   @Test
-  public void newEvent_returnsFood_Lots() {
-    String expectedOutput = "Lots";
+  public void newEvent_returnsFood_Snacks() {
+    String expectedOutput = "Snacks";
     assertEquals(expectedOutput, newEvent.getFood());
   }
   @Test
