7891bcf61fe86cfb0ac8e67d9c39cc44ab920960
Ben Vissotzky <ben.vissotzky@gmail.com>
2016-09-09 10:03:12 -0700

    Add input for other additions to event

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,41 @@
 public class Event {
   private Integer mGuests;
+  private String mVenue;
+  private boolean mAlcohol;
+  private String mBand;
+  private String mFood;
+  private Integer mDuration;
+  private Integer mEventCost;
 
-  public Event(Integer _userGuests) {
-    mGuests = _userGuests;
+  public Event(Integer _guests, String _venue, boolean _alcohol, String _band, String _food, Integer _duration) {
+    mGuests = _guests;
+    mVenue = _venue;
+    mAlcohol = _alcohol;
+    mBand = _band;
+    mFood = _food;
+    mDuration = _duration;
   }
+
+
+
 
   public Integer getGuests() {
     return mGuests;
   }
+  public String getVenue() {
+    return mVenue;
+  }
+  public boolean getAlcohol() {
+    return mAlcohol;
+  }
+  public String getBand() {
+    return mBand;
+  }
+  public String getFood() {
+    return mFood;
+  }
+  public Integer getDuration() {
+    return mDuration;
+  }
+
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -4,16 +4,39 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class EventTest {
-
+  Event newEvent = new Event(100,"Patio",true,"Ruckers","Lots",60);
   @Test
   public void newEvent_instantiatesCorrectly_True() {
-    Event newEvent = new Event(100);
     assertEquals(true, newEvent instanceof Event);
   }
   @Test
-  public void newEvent_returnsGuestNumber_True() {
-    Event newEvent = new Event(100);
+  public void newEvent_returnsGuestNumber_100() {
     Integer expectedOutput = 100;
     assertEquals(expectedOutput, newEvent.getGuests());
   }
+  @Test
+  public void newEvent_returnsVenue_Patio() {
+    String expectedOutput = "Patio";
+    assertEquals(expectedOutput, newEvent.getVenue());
+  }
+  @Test
+  public void newEvent_returnsAlcohol_True() {
+    boolean expectedOutput = true;
+    assertEquals(expectedOutput, newEvent.getAlcohol());
+  }@Test
+  public void newEvent_returnsBand_Ruckers() {
+    String expectedOutput = "Ruckers";
+    assertEquals(expectedOutput, newEvent.getBand());
+  }
+  @Test
+  public void newEvent_returnsFood_Lots() {
+    String expectedOutput = "Lots";
+    assertEquals(expectedOutput, newEvent.getFood());
+  }
+  @Test
+  public void newEvent_returnsDuration_60() {
+    Integer expectedOutput = 60;
+    assertEquals(expectedOutput, newEvent.getDuration());
+  }
+
 }
